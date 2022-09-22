0930c65ea5f4ec34fe0b965a0078e6afb5285440
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-09-22 12:48:43 -0300

    Update novo.c

--- novo.c
+++ novo.c
@@ -22,3 +22,72 @@ int main(){
     printf("O módulodo primeiro número  %.3lf\n",valor1<0 ?  valor1*-1:  valor1 );
     
 }
+---------------------------------------------------------
+  /*2*/
+    
+    #include <stdio.h>
+
+int main()
+{
+    float temperatura,Celsius,Kelvin,calculo;
+    printf("digite a temperatura que voce deseja: ");
+    scanf("%f",&temperatura);
+    
+    Celsius=((temperatura-32)*5)/9;
+    Kelvin=  273.15 + Celsius;
+    printf("Fahrenheit %.3f,Celsius %.2f,Kelvin %.3f",temperatura,Celsius,Kelvin);
+
+}
+-----------------------------------------------------------
+    /*3*/
+    #include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("digite um numero decimal: ");
+    scanf("%d",&num);
+    
+    
+    printf("octal: %o\nhexadecimal: %X",num,num);
+
+    return 0;
+}
+-------------------------------------------------------------------
+    /*4*/
+    
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("digie dois valores: ");
+    scanf("%d %d",&num1,&num2);
+    
+    if (num1 < num2){
+        for(int i = num1; i<num2;i++){
+            printf("%d - ",i);
+        }
+        printf("%d",num2);
+    }else if(num1 > num2) {
+        for(int i = num1; i>num2;i--){
+            printf("%d - ",i);
+        }
+        printf("%d",num2);
+    }else{
+        printf("valores iguais");
+    }
+
+    return 0;
+}
+-------------------------------------------------------
+/*5*/
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0; i <128;i++){
+        printf("código decimal %d| código hexa %x| caractere %c\n",i,i,i);
+    }
+}
